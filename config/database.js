290463530dc0910d655c95d227a48761ae34f33d
Daniel Guilger <daniel.guilger@sou.inteli.edu.br>
2025-06-06 07:30:51 -0300

    feat: adiciona a pasta config e o database.js

---
+++ config/database.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+class Database {
+    constructor() {
+        this.pool = new Pool({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            port: process.env.DB_PORT
+        });
+    }
+
+    async query(text, params) {
+        return this.pool.query(text, params);
+    }
+}
+
+module.exports = new Database();
